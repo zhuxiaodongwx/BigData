b35ed072eec6fb1f19bdb73c944d5622fb3078ce
zhuxiaodongwx <zhuxiaodongwx@126.com>
2021-02-07 04:26:34 +0800

    1、添加vmware集群启动脚本 2、hadoop hdfs shell管理文件命令笔记完善

---
+++ Code/03_Framework/04_Shell/CommonShell/Batch/hadoop集群__停止.bat
@@ -0,0 +1,3 @@
+vmrun stop "D:\DevelopTools\BigDataVM\Hadoop102\Hadoop102.vmx" soft
+vmrun stop "D:\DevelopTools\BigDataVM\Hadoop103\Hadoop103.vmx" soft
+vmrun stop "D:\DevelopTools\BigDataVM\Hadoop104\Hadoop104.vmx" soft
---
+++ Code/03_Framework/04_Shell/CommonShell/Batch/hadoop集群__启动.bat
@@ -0,0 +1,3 @@
+vmrun start "D:\DevelopTools\BigDataVM\Hadoop102\Hadoop102.vmx" gui
+vmrun start "D:\DevelopTools\BigDataVM\Hadoop103\Hadoop103.vmx" gui
+vmrun start "D:\DevelopTools\BigDataVM\Hadoop104\Hadoop104.vmx" gui
