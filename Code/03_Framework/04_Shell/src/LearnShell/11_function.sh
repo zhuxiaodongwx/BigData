c0de2131e6250d48e57f2a5120f760a4a221e6b0
zhuxiaodongwx <zhuxiaodongwx@126.com>
2021-03-15 08:30:42 +0800

    shell命令学习  sed awk

--- Code/03_Framework/04_Shell/src/LearnShell/11_function.sh
+++ Code/03_Framework/04_Shell/src/LearnShell/11_function.sh
@@ -11,4 +11,6 @@ function sum() {
 
 read -p "请输入第一个数：" num1
 read -p "请输入第二个数：" num2
+
+# 函数调用
 sum $num1 $num2
---
+++ Code/03_Framework/04_Shell/src/LearnShell/12_cut_sed_awk_sort.sh
@@ -0,0 +1,36 @@
+#!/bin/batch
+
+# cut工具
+
+# 以：为分隔符，获取第二行
+cut -f 2 -d : /root/learnShell/file/12.txt
+
+# 获取每行的第一个字符
+cut -c 1 /root/learnShell/file/12.txt
+
+# ----------- sed工具 -----------
+
+# 每一行后面添加xiaodong
+sed -e 'axiaodong' 12.txt
+
+# 在第二行后面添加xiaodong
+sed -e '2axiaodong' 12.txt
+
+# 将所有的81替换为99
+sed -e 's/81/99/g' 12.txt
+
+# 将含有81的所有行删除
+sed -e '/81/d' 12.txt
+
+# ----------- awk工具 -----------
+
+#以：为切割符号，将第三列的数据加10并输出
+awk -F : -v i=10 '{print $3+i}' 12.txt
+
+# ----------- sort工具 -----------
+awk -F : '/^dbus/{print"这一行以root开头！第一列的值是："$1} /^x/{print"这一行以b开头，第一行是"$1}' 12.txt
+
+# 数据处理前先打印开始处理，处理完成后打印处理结束
+# 以：拆分每一列，打印输出第1列和第3列
+awk -F : 'BEGIN{print "开始处理"} {print "第一列是："$1 "第三列的值是" $3}  END{print "处理结束"}' 12.txt
+
