34ead37c300094d508d277e294d205c055fdd790
zhuxiaodongwx <zhuxiaodongwx@126.com>
2021-03-31 06:48:18 +0800

    mapjoin案例

---
+++ Code/03_Framework/05_Hadoop/MapReduce01/src/main/java/cn/xiaodong/join/mapJoin/OrderMapJoinDriver.java
@@ -0,0 +1,76 @@
+package cn.xiaodong.join.mapJoin;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * @description: ReduceJoin驱动类
+ * @author: xiaodong
+ * @create: 2021-03-30 06:33
+ **/
+public class OrderMapJoinDriver {
+
+    /**
+     * 设置任务属性
+     *
+     * @param args
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws Exception {
+        // 设置连接Hadoop的用户名
+        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+        // 0、系统设置
+        Configuration configuration = new Configuration();
+
+        // 数据源使用HDFS
+        configuration.set("fs.defaultFS", "hdfs://hadoop102:8020");
+        // 数据源使用本地磁盘
+        // configuration.set("fs.defaultFS", "file:///");
+
+        // 此处写local表示程序在本地上运行，可以进行断点调试
+        configuration.set("mapreduce.framework.name", "local");
+        // 此处写yarn表示程序在集群上运行，yarn负责调度，无法进行断点调试
+        // configuration.set("mapreduce.framework.name","yarn");
+
+        configuration.set("mapreduce.app-submission.cross-platform", "true");
+        configuration.set("yarn.resourcemanager.hostname", "hadoop103");
+
+        // 1.获取Job
+        Job job = Job.getInstance(configuration);
+
+        // 2.设置driver类、map类、reduce类
+//        job.setJarByClass(WordCountDriver.class);
+        job.setJar("D:\\Code\\LearningBigData\\Code\\03_Framework\\05_Hadoop\\MapReduce01\\target\\MapReduce01-1.0-SNAPSHOT.jar");
+
+        job.setMapperClass(OrderMapper.class);
+        job.setNumReduceTasks(0);
+
+        // 添加分布式缓存文件
+        // job.addCacheArchive(URI.create("file:///txt/reduceJoin/pd.txt"));
+
+        // 3.设置map、reduce的输出
+        // mapper的输出格式
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        // 4、设置文件输入输出路径
+        FileInputFormat.setInputPaths(job, new Path("/txt/reduceJoin/order.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("/txt/reduceJoinOutput"));
+
+        // 5.任务提交
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+    }
+}
---
+++ Code/03_Framework/05_Hadoop/MapReduce01/src/main/java/cn/xiaodong/join/mapJoin/OrderMapper.java
@@ -0,0 +1,73 @@
+package cn.xiaodong.join.mapJoin;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+
+/**
+ * @description: OrderMapJoin map处理类
+ * @author: xiaodong
+ * @create: 2021-03-31 06:12
+ **/
+public class OrderMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+
+    /**
+     * 产品信息，key是产品id，value是产品名字
+     */
+    private HashMap product = new HashMap<String, String>();
+
+    /**
+     * 将商品文件从分布式文件缓存中读取出来，然后处理成KV值的形式，放到文件内存中
+     *
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        // 获取分布式缓存文件
+        // URI[] cacheArchives = context.getCacheArchives();
+
+        // 开文件流
+        Configuration conf;
+        FileSystem fileSystem = FileSystem.get(context.getConfiguration());
+//        FSDataInputStream fsDataInputStream = fileSystem.open(new Path(cacheArchives[0]));
+        FSDataInputStream fsDataInputStream = fileSystem.open(new Path("/txt/reduceJoin/pd.txt"));
+
+        // 将hdfs数据流转换成缓存字符流
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fsDataInputStream));
+        String line;
+        while (StringUtils.isNotEmpty(line = bufferedReader.readLine())) {
+            String[] split = line.split("\t");
+            product.put(split[0], split[1]);
+        }
+
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // 将每一行的输入拆分
+        String[] split = value.toString().split("\t");
+
+        // 将每一行的第二列pid换成商品名称
+        String pname = (String) product.get(split[1]);
+
+        // 组装新的一行
+        String newLine = split[0] + "\t" + pname + "\t" + split[2];
+
+        // 返回处理结果
+        context.write(new Text(newLine), NullWritable.get());
+    }
+}
