d6f0e69c1cd53081118348b4e3967e6e348ba20b
zhuxiaodongwx <zhuxiaodongwx@126.com>
2021-05-23 19:22:09 +0800

    hive企业优化

--- Code/03_Framework/08_Hive/HQL/06_hqlOptimize.sql
+++ Code/03_Framework/08_Hive/HQL/06_hqlOptimize.sql
@@ -9,3 +9,9 @@ set hive.exec.mode.local.auto;
 -- 设置本地模式
 set hive.exec.mode.local.auto = true;
 
+
+// =============== 并行执行  ===============
+--打开任务并行执行
+set hive.exec.parallel=true;
+--同一个sql允许最大并行度，默认为8。
+set hive.exec.parallel.thread.number=16;
